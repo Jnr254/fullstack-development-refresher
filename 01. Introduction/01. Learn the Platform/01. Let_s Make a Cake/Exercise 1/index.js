7bf4a0fd6ec1bf1c0e07a6f789349bc39f785ffa
Jnr254 <juniorjulius81@gmail.com>
2026-04-16 22:48:38 +0300

    Changed the display color and the hover effect: Requirements for exercise 1

--- 01. Introduction/01. Learn the Platform/01. Let_s Make a Cake/Exercise 1/index.js
+++ 01. Introduction/01. Learn the Platform/01. Let_s Make a Cake/Exercise 1/index.js
@@ -1,13 +1,13 @@
 function setTheme(theme) {
     let root = document.documentElement;
     if (theme === 'light') {
-        root.style.setProperty('--bg-color', '#ECF2FF');
-        root.style.setProperty('--text-color', '#2B283A');
-        root.style.setProperty('--title-color', '#4A4E74');        
+        root.style.setProperty('--bg-color', '#ffffff');
+        root.style.setProperty('--text-color', '#0c5dea');
+        root.style.setProperty('--title-color', '#0c5dea');        
     } else if (theme === 'dark') {
         root.style.setProperty('--bg-color', '#2B283A');
         root.style.setProperty('--text-color', '#ECF2FF');
-        root.style.setProperty('--title-color', '#D5D4D8');
+        root.style.setProperty('--title-color', '#D5D4D8');   
     }
 }
 
